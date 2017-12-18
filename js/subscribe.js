e1b476234cb86d306348bdb28b35298ead803b89
Kevin <n.nkr3110@gmail.com>
2017-12-17 19:34:16 -0500

    reducing time and adding else for loop
    The text animation was breaking because the logic of the loop was checking length while it was undefined.

--- js/subscribe.js
+++ js/subscribe.js
@@ -64,7 +64,7 @@ document.addEventListener('DOMContentLoaded',function(event){
      if (typeof dataText[i] == 'undefined'){
         setTimeout(function() {
           StartTextAnimation(0);
-        }, 20000);
+        }, 5000);
      }
      // check if dataText[i] exists
    else if (i < dataText[i].length) {
